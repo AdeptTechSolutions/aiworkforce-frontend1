7ad51571542f30990535ded61f4d15d7d31c814b
SikandarATS <100119589+SikandarATS@users.noreply.github.com>
2026-02-11 00:57:10 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,15 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: [
+      'aiworkforce-prod-alb-240576262.us-east-1.elb.amazonaws.com'
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://aiworkforce-prod-alb-240576262.us-east-1.elb.amazonaws.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 })
